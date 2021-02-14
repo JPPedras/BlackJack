a08da5eda48cef7a4593bea7b73d474913840785
João Pedras <joaoliveirapedras@gmail.com>
2021-02-14 17:17:50 +0000

    Done lol

--- game.py
+++ game.py
@@ -1,19 +1,81 @@
 import utils
 import time
+import sys
 import random as rd
 
-cards=['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+cards=['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 
 print('Welcome to BlackJack!')
 print("Let's start!\n")
 
-player_hand=[cards[rd.randint(0,13)]]
+player_hand=[cards[rd.randint(0,12)]]
 print('Your Hand:')
 utils.draw_cards(player_hand)
 print('')
 time.sleep(2)
-dealer_hand=[cards[rd.randint(0,13)]]
+
+dealer_hand=[cards[rd.randint(0,12)]]
 print("Dealer's Hand:")
 utils.draw_cards(dealer_hand)
+print('')
+time.sleep(2)
+
+player_hand.append(cards[rd.randint(0,12)])
+print('Your Hand:')
+utils.draw_cards(player_hand)
+print('')
+time.sleep(2)
+
+if utils.get_score(player_hand) == 21:
+    print('YOU GOT BLACKJACK AND WON!')
+    sys.exit()
+
+dealer_hand.append('?')
+print("Dealer's Hand:")
+utils.draw_cards(dealer_hand)
+print('')
+
+    
+choice=input("'H' to hit or 'S' to stand: ")
+while choice == 'H':
+
+    player_hand.append(cards[rd.randint(0,12)])
+    print('Your Hand:')
+    utils.draw_cards(player_hand)
+    print('')
+    time.sleep(2)
+
+    if utils.get_score(player_hand) > 21:
+        print('You Busted! Game Over!')
+        sys.exit()
+
+    choice=input("'H' to hit or 'S' to stand: ")
+
+time.sleep(2)
+dealer_hand[1]=cards[rd.randint(0,12)]
+print("Dealer's Hand:")
+utils.draw_cards(dealer_hand)
+print('')
+time.sleep(2)
+
+while utils.get_score(dealer_hand) < 17:
+
+    dealer_hand.append(cards[rd.randint(0,12)])
+    print("Dealer's Hand:")
+    utils.draw_cards(dealer_hand)
+    print('')
+    time.sleep(2)
+
+if utils.get_score(dealer_hand) > 21:
+        print('Dealer Busted! YOU WON!')
+        sys.exit()
+
+time.sleep(2)
+if utils.get_score(player_hand) > utils.get_score(dealer_hand):
+    print('YOU WON! GONGRATS')
+elif utils.get_score(player_hand) < utils.get_score(dealer_hand):
+    print('YOU LOST! GAME OVER!')
+else:
+    print("IT'S A DRAW!")
 
     
--- utils.py
+++ utils.py
@@ -6,9 +6,32 @@ def draw_cards(hand):
         print('|   | ', end='')
     print('')
     for card in hand:
-        print(f'| {card} | ', end='')
+        if card != 10:
+            print(f'| {card} | ', end='')
+        else:
+            print(f'| {card}| ', end='')
     print('')
     for card in hand:
         print('|___| ', end='')
-    print('') 
+    print('')
+
+def get_score(hand):
+    score1=0
+    score2=0
+    for card in hand:
+        if card == 'A':
+            score1 += 11
+            score2 += 1
+        elif type(card) == int:
+            score1 += card
+            score2 += card
+        else:
+            score1 += 10
+            score2 += 10
+    if score1 > 21:
+        return score2
+    else:
+        return score1
+
+    
 
